5b733abe43690c0460f24f939f873ba069389c37
Dmitry Denisenko <dmitryesten@gmail.com>
2017-09-11 19:22:36 +0600

    Wrote unit tests for class ElementaryMathOperation

--- src/test/java/net/thumbtack/denisenko/trainee/introduction/ElementaryMathOperationTest.java
+++ src/test/java/net/thumbtack/denisenko/trainee/introduction/ElementaryMathOperationTest.java
@@ -6,11 +6,61 @@ import org.junit.Test;
 import org.junit.*;
 
 public class ElementaryMathOperationTest {
-    ElementaryMathOperation e
+    ElementaryMathOperation elementaryMathOperation = new ElementaryMathOperation();
 
     @Test
-    public void adder(){
-        assertEquals(4, 8);
+    public void testAdder() {
+        assertEquals(4, elementaryMathOperation.adder(2, 2));
+        assertEquals(2, elementaryMathOperation.adder(2, -2));
+        assertEquals(2, elementaryMathOperation.adder(-2, 2));
+        assertEquals(-4, elementaryMathOperation.adder(-2, -2));
+        assertNotEquals(1, elementaryMathOperation.adder(0, 0));
     }
 
-}
+    @Test
+    public void testMulty(){
+        assertEquals(4, elementaryMathOperation.multy(2,2));
+        assertEquals(-4, elementaryMathOperation.multy(2, -2));
+        assertEquals(-4, elementaryMathOperation.multy(-2, 2));
+        assertEquals(4, elementaryMathOperation.multy(-2, -2));
+        assertEquals(0, elementaryMathOperation.multy(2, 0));
+    }
+
+    @Test
+    public void testDevision() throws ArithmeticException{
+        assertEquals("2", elementaryMathOperation.devision(4,2));
+        assertEquals("-2", elementaryMathOperation.devision(4,-2));
+        assertEquals("-2", elementaryMathOperation.devision(-4,2));
+        assertEquals("2", elementaryMathOperation.devision(-4,-2));
+
+        assertEquals("0", elementaryMathOperation.devision(0,-2));
+        assertEquals("2", elementaryMathOperation.devision(0,2));
+
+        assertEquals("На 0 делить нельзя", elementaryMathOperation.devision(4,0));
+
+    }
+
+    @Test
+    public void testRemainderOfDivision() throws ArithmeticException{
+        assertEquals("2", elementaryMathOperation.remainderOfDivision(8,3));
+        assertEquals("-2", elementaryMathOperation.remainderOfDivision(-8,3));
+        assertEquals("2", elementaryMathOperation.remainderOfDivision(8,-3));
+
+        assertEquals("На 0 делить нельзя", elementaryMathOperation.remainderOfDivision(8,0));
+
+    }
+
+    @Test
+    public void testCompareValues(){
+        int comparingValueOne = 4;
+        int comparingValueTwo = 2;
+        int comparingValueOneForLess = 2;
+        int comparingValueTwoForLess = 4;
+        assertEquals("Два числа равны", elementaryMathOperation.compareValues(4, 4));
+        assertEquals(comparingValueTwo + " больше, чем "+ comparingValueOne, elementaryMathOperation.compareValues(comparingValueOne, comparingValueTwo));
+        assertEquals(comparingValueTwoForLess + " менььше, чем "+ comparingValueOneForLess, elementaryMathOperation.compareValues(comparingValueOneForLess, comparingValueTwoForLess));
+
+    }
+
+
+    }
