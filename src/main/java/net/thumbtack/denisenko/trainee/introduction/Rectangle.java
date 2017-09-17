f966ecaa8822c999f12553e10f882b5e11fbc619
Dmitry Denisenko <dmitryesten@gmail.com>
2017-09-17 14:29:20 +0600

    Created some methods(constructors, printCoordinats, area, isPointContainedRect)

--- src/main/java/net/thumbtack/denisenko/trainee/introduction/Rectangle.java
+++ src/main/java/net/thumbtack/denisenko/trainee/introduction/Rectangle.java
@@ -5,19 +5,24 @@ public class Rectangle {
     private Point2D leftDownPoint = new Point2D( 1, 3);
     private Point2D rightUpPoint = new Point2D(5, 5);
 
+    private int width;
+    private int height;
+
+
     private int xLeftDownAngle;
     private int yLeftDownAngle;
 
-    private int xRightDownAngle;
-    private int yRightDownAngle;
-
     private int xRightUpAngle;
     private int yRightUpAngle;
+
+    private int xRightDownAngle;
+    private int yRightDownAngle;
 
     private int xLeftUpAngle;
     private int yLeftUpAngle;
 
     Rectangle(){
+        this(1, 1);
         Point2D leftDownPoint = new Point2D();
         this.xLeftDownAngle = leftDownPoint.setAbscissaOx(0);
         this.yLeftDownAngle = leftDownPoint.setOrdinateOy(0);
@@ -34,9 +39,140 @@ public class Rectangle {
     }
 
 
-    Rectangle(Point2D rightUpPoint) {
-        Point2D point2D = new Point2D(0, 0);
+    Rectangle(int width, int height) {
+        Point2D firstPoint = new Point2D();
+        Point2D secoundPoint = new Point2D();
+        this.xLeftDownAngle = firstPoint.getAbscissaOx();
+        this.yLeftDownAngle = firstPoint.getOrdinateOy();
+
+        this.xRightUpAngle = secoundPoint.setAbscissaOx(width);
+        this.yRightUpAngle = secoundPoint.setOrdinateOy(height);
+
     }
+
+
+
+    public String printCoordinatesRactangle(Rectangle rect) {
+        return  "Left-Down angle: ("+rect.getXLeftDownAngle()+ ", " + rect.getYLeftDownAngle()+") " + "\r\n" +
+                "Right-Down angle: ( "+ rect.getXRightUpAngle()+ ", " + rect.getYLeftDownAngle()+ ") " + "\r\n"+
+                "Right-Up angle: ( "+ rect.getXRightUpAngle()+ ", " + rect.getYRightUpAngle()+ ") " + "\r\n" +
+                "Left-Up angle: ( "+ rect.getXLeftDownAngle()+ ", " + rect.getYRightUpAngle()+ ") " + "\r\n";
+    }
+
+    public void moveRectangale(int newX, int newY){
+        xLeftDownAngle += newX;
+        yLeftDownAngle += newY;
+
+        xRightUpAngle += newX;
+        yRightUpAngle += newY;
+
+        xRightDownAngle +=newX;
+        yRightDownAngle +=newY;
+
+        xRightUpAngle += newX;
+        yRightUpAngle += newY;
+    }
+
+    public void reduceRactangle(int valueReduceX, int valueReduceY ){
+        xRightDownAngle -=valueReduceX;
+
+        xRightUpAngle -= valueReduceX;
+        yRightUpAngle -= valueReduceY;
+
+        yLeftUpAngle -= valueReduceY;
+    }
+
+
+    public int areaRectangle(Rectangle rect){
+        return (rect.getXRightDownAngle() - rect.getXLeftDownAngle()) * (rect.getYLeftUpAngle() - rect.getYLeftDownAngle());
+    }
+
+    public boolean isPointContainedInRactangle (Point2D point, Rectangle rectangle){
+
+        int coordPointX = point.getAbscissaOx();
+        int coordPointY = point.getOrdinateOy();
+
+        return ((coordPointX >= rectangle.getXLeftDownAngle() &&
+                coordPointX <= rectangle.getXRightDownAngle())
+                &&
+                (coordPointY >=rectangle.getYLeftDownAngle() &&
+                coordPointY<=rectangle.getYLeftUpAngle())) ? true : false;
+    }
+
+
+
+    public int getXRightDownAngle() {
+        return xRightDownAngle;
+    }
+
+    public int getYLeftUpAngle() {
+        return yLeftUpAngle;
+    }
+
+    public void setYLeftUpAngle(int yLeftUpAngle) {
+        this.yLeftUpAngle = yLeftUpAngle;
+    }
+
+    public int getXLeftUpAngle() {
+
+        return xLeftUpAngle;
+    }
+
+    public void setXLeftUpAngle(int xLeftUpAngle) {
+        this.xLeftUpAngle = xLeftUpAngle;
+    }
+
+    public int getYRightDownAngle() {
+
+        return yRightDownAngle;
+    }
+
+    public void setYRightDownAngle(int yRightDownAngle) {
+        this.yRightDownAngle = yRightDownAngle;
+    }
+
+    public void setXRightDownAngle(int xRightDownAngle) {
+        this.xRightDownAngle = xRightDownAngle;
+    }
+
+
+
+
+
+    //Left down angle
+    public int getXLeftDownAngle() {
+        return xLeftDownAngle;
+    }
+
+    public void setXLeftDownAngle(int xLeftDownAngle) {
+        this.xLeftDownAngle = xLeftDownAngle;
+    }
+
+    public int getYLeftDownAngle() {
+        return yLeftDownAngle;
+    }
+
+    public void setYLeftDownAngle(int yLeftDownAngle) {
+        this.yLeftDownAngle = yLeftDownAngle;
+    }
+
+    //Right Up angle
+    public int getXRightUpAngle() {
+        return xRightUpAngle;
+    }
+
+    public void setXRightUpAngle(int xRightUpAngle) {
+        this.xRightUpAngle = xRightUpAngle;
+    }
+
+    public int getYRightUpAngle() {
+        return yRightUpAngle;
+    }
+
+    public void setYRightUpAngle(int yRightUpAngle) {
+        this.yRightUpAngle = yRightUpAngle;
+    }
+
 
 
 
