aee359557e40dc45636206849b7ffc3f2aec3e81
Dmitry Denisenko <dmitryesten@gmail.com>
2017-09-21 01:11:47 +0600

    Wrote junit test

--- src/test/java/net/thumbtack/denisenko/trainee/introduction/Point2DTest.java
+++ src/test/java/net/thumbtack/denisenko/trainee/introduction/Point2DTest.java
@@ -5,16 +5,22 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class Point2DTest {
-    Point2D point = new Point2D(2,2);
+    Point2D point2D = new Point2D(2,2);
 
     @Test
     public void movePoint() throws Exception {
+        assertEquals(1 , point2D.getAbscissaOx());
+        assertEquals(1, point2D.getOrdinateOy());
 
-       // assertEquals(2, point.movePoint());
+        point2D.movePoint(1, 1);
+
+        assertEquals(2, point2D.getAbscissaOx());
+        assertEquals(2, point2D.getOrdinateOy());
     }
 
     @Test
     public void printCoordinates() throws Exception {
+        assertEquals("OX: "+ 1 +"; OY: " + 1 , Point2D.printCoordinates(point2D));
     }
 
 }
