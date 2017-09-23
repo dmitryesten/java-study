64de978c50a50ece0ead838f37b4d7ce3cda99ba
Dmitry Denisenko <dmitryesten@gmail.com>
2017-09-23 18:42:27 +0600

    Added new class for calling result

--- src/main/java/net/thumbtack/denisenko/trainee/Trainee.java
+++ src/main/java/net/thumbtack/denisenko/trainee/Trainee.java
@@ -1,6 +1,14 @@
 package net.thumbtack.denisenko.trainee;
 
-import net.thumbtack.denisenko.trainee.introduction.*;
+import net.thumbtack.denisenko.trainee.introduction.task1.HelloWorld;
+import net.thumbtack.denisenko.trainee.introduction.task10.Circle;
+import net.thumbtack.denisenko.trainee.introduction.task11.CircleFactory;
+import net.thumbtack.denisenko.trainee.introduction.task2.MathOperation;
+import net.thumbtack.denisenko.trainee.introduction.task3.FloatingOperations;
+import net.thumbtack.denisenko.trainee.introduction.task5.ArrayOperations;
+import net.thumbtack.denisenko.trainee.introduction.task6.CheckSortingArray;
+import net.thumbtack.denisenko.trainee.introduction.task7.Point2D;
+import net.thumbtack.denisenko.trainee.introduction.task8.Rectangle;
 
 import java.util.Random;
 import java.util.Scanner;
@@ -17,9 +25,10 @@ public class Trainee
     public static void main( String[] args ) throws Throwable {
 
         HelloWorld stringHelloWorld = new HelloWorld();
-        ElementaryMathOperation elementaryMathOperation = new ElementaryMathOperation();
-        OperationsAboveElementsOfArray operationsAboveElementsOfArray = new OperationsAboveElementsOfArray();
+        MathOperation elementaryMathOperation = new MathOperation();
+        ArrayOperations operationsAboveElementsOfArray = new ArrayOperations();
         CheckSortingArray checkSortingArray = new CheckSortingArray();
+        FloatingOperations floatingOperations = new FloatingOperations();
 
 
         Scanner in = new Scanner(System.in);
@@ -48,6 +57,8 @@ public class Trainee
 
 
         System.out.println("\r\nTask_4");
+        System.out.println("Сумма вещественных:"+ FloatingOperations.add(0.09, 0.011));
+        System.out.println("Сравнение вещественных:"+ FloatingOperations.compareValues(0.09, 2.09));
 
         System.out.println("----------Task_5-----------------------------" );
         randomValue(array);
@@ -64,32 +75,35 @@ public class Trainee
         System.out.println("Среднее значение: " + operationsAboveElementsOfArray.middleValue(array, operationsAboveElementsOfArray.amountAllValues(array) ) );
 
 
+
         System.out.println("\r\n----------Task_6-----------------------------" );
         //Task_5
         System.out.println("Массив возрастающий: " + checkSortingArray.checkAscendingArray(array));
         System.out.println("Массив убывающий: " + checkSortingArray.checkDescendingArray(array));
 
-
+/*
         System.out.println("\r\n----------Task_7-----------------------------" );
         Point2D point2D = new Point2D(1, 2);
         System.out.println("Начальная координата: " + Point2D.printCoordinates(point2D) );
-        point2D.movePoint(2,4);
+        point2D.move(2,4);
         System.out.println("Точка перемещена:" + Point2D.printCoordinates(point2D) );
-
+*/
 
         System.out.println("\r\n----------Task_8 & Task_4-----------------------------" );
         //Task_8
-        Point2D point1 = new Point2D(1, 2);
-        Point2D point2 = new Point2D(3, 4);
-        //Point2D point3 = new Point2D(4, 1);
-        //Point2D point4 = new Point2D(6, 2);
-        Point2D point3 = new Point2D(2, 3);
-        Point2D point4 = new Point2D(4, 5);
-        Rectangle rectangle = new Rectangle(point1, point2);
-        Rectangle rectangle2 = new Rectangle(point3, point4);
-        System.out.println( Rectangle.printCoordinatesRactangle(rectangle) );
+
+        Point2D topLeft1 = new Point2D(1, 5);
+        Point2D bottonRight2 = new Point2D(7, 2);
+        //Point2D topLeft3 = new Point2D(8, 6);
+        //Point2D bottonRight4 = new Point2D(9, 1);
+        Point2D topLeft3 = new Point2D(2, 4);
+        Point2D bottonRight4 = new Point2D(6, 3);
+        Rectangle rectangle = new Rectangle(topLeft1, bottonRight2);
+        Rectangle rectangle2 = new Rectangle(topLeft3, bottonRight4);
+        System.out.println( Rectangle.printCoordinatesRectangle(rectangle) );
+        System.out.println("\r\n"+ Rectangle.printCoordinatesRectangle(rectangle2) );
         System.out.println("Площадь = " + Rectangle.areaRectangle(rectangle) );
-        //rectangle.moveRectangale(1,1);
+        //rectangle.move(1,1);
         //System.out.println("Перемещенин треугольника:\r\n"+ Rectangle.printCoordinatesRactangle(rectangle) );
         //rectangle.reduceRectangle(1,1);
         //System.out.println("Уменьшение треугольника:\r\n"+ Rectangle.printCoordinatesRactangle(rectangle)+"\r\n" );
@@ -98,8 +112,16 @@ public class Trainee
         System.out.println("Два прямоугольнрка пересекаются:"+Rectangle.isRectangleCrossingWithOtherRectangle(rectangle, rectangle2));
 
 
+        System.out.println(Rectangle.printCoordinatesRectangle( Rectangle.large(2,rectangle)) );
 
-
+        System.out.println("\r\n----------Task_11-----------------------------" );
+        Point2D point2D = new Point2D(1,1);
+        CircleFactory circleFactory = new CircleFactory(point2D, 3);
+        System.out.println("i ="+ CircleFactory.getNumberInstances() );
+        CircleFactory circleFactory2 = new CircleFactory(point2D, 3);
+        System.out.println("i ="+ CircleFactory.getNumberInstances() );
+        CircleFactory circleFactory3 = new CircleFactory(point2D, 3);
+        System.out.println("i ="+ CircleFactory.getNumberInstances() );
 
 
 
