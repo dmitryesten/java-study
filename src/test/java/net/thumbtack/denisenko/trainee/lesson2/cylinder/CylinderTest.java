104f81d79e4b2d7976047c9b47e8e17a96e3b400
Дмитрий Д <dmitryesten@gmail.com>
2017-09-28 10:16:10 +0000

    Add new file CylinderTest

---
+++ src/test/java/net/thumbtack/denisenko/trainee/lesson2/cylinder/CylinderTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CylinderTest {
+    @Test
+    public void printCoordinates() throws Exception {
+        Cylinder cylinder = new Cylinder(5, 5,3, 4);
+
+        assertEquals(5, cylinder.getX());
+        assertEquals(5, cylinder.getY());
+        assertEquals(3, cylinder.getRadius());
+        assertEquals(4, cylinder.getHeight());
+    }
+
+
+    @Test
+    public void areaBase() throws Exception {
+        Cylinder cylinder = new Cylinder(5, 5,3, 4);
+        assertEquals(Math.PI *3*3, Cylinder.areaBase(cylinder), 0.001);
+    }
+
+    @Test
+    public void volume() throws Exception {
+        Cylinder cylinder = new Cylinder(5, 5,3, 4);
+        assertEquals(Math.PI *3*3*4, Cylinder.volume(cylinder), 0.001);
+    }
+
+    @Test
+    public void move() throws Exception {
+        Cylinder cylinder = new Cylinder(5, 5,3, 4);
+        cylinder.move(2, 2);
+        assertEquals(7, cylinder.getX());
+        assertEquals(7, cylinder.getY());
+    }
+
+}
