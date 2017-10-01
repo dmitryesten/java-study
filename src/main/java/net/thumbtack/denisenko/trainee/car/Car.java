b02d995c38e55787302d34d48ecc3bbdca886c2f
Dmitry Denisenko <dmitryesten@gmail.com>
2017-10-01 15:36:53 +0600

    Create new class

---
+++ src/main/java/net/thumbtack/denisenko/trainee/car/Car.java
@@ -0,0 +1,68 @@
+package net.thumbtack.denisenko.trainee.car;
+
+import net.thumbtack.denisenko.trainee.Colored;
+import java.util.Objects;
+
+public class Car implements Colored {
+
+    String mark;
+    int weight;
+    int maxSpeed;
+
+    Car(String mark, int weight, int maxSpeed){
+        this.mark = mark;
+        this.weight = weight;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public String printInfoCar(){
+        return "Марка: "+ getMark() + "; Вес: "+getWeight()+"кг; MaxСкорость: "+getMaxSpeed() +"км/ч";
+    }
+
+
+    public String getMark() {
+        return mark;
+    }
+
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+
+    public int getWeight() {
+        return weight;
+    }
+
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car)) return false;
+        Car car = (Car) o;
+        return weight == car.weight &&
+                maxSpeed == car.maxSpeed &&
+                Objects.equals(mark, car.mark);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mark, weight, maxSpeed);
+    }
+}
