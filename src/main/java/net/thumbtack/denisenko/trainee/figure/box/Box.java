6a5d8a6e96cb9f1fb6dc67573d91472b3fd01e73
Dmitry Denisenko <dmitryesten@gmail.com>
2017-10-04 14:07:16 +0600

    Creat Box class

--- src/main/java/net/thumbtack/denisenko/trainee/figure/box/Box.java
+++ src/main/java/net/thumbtack/denisenko/trainee/figure/box/Box.java
@@ -2,6 +2,19 @@ package net.thumbtack.denisenko.trainee.figure.box;
 
 public class Box <T> {
 
+    private T content;
 
+    Box(T content){
+        this.content = content;
+    }
+
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
 
 }
