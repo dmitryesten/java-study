8e9ab0562533579356b16c5f365279a9aa8e0d1a
Dmitry Denisenko <dmitryesten@gmail.com>
2017-10-05 23:51:41 +0600

    Create ArrayBox class

---
+++ src/main/java/net/thumbtack/denisenko/trainee/figure/box/ArrayBox.java
@@ -0,0 +1,11 @@
+package net.thumbtack.denisenko.trainee.figure.box;
+
+public class ArrayBox<T> {
+
+    private T [] content;
+
+    public ArrayBox(T [] content) {
+        this.content = content;
+    }
+
+}
