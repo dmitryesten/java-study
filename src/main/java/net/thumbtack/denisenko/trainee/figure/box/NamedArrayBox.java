192ebf94e83b750837dcf0684cf43266f58ad9f7
Dmitry Denisenko <dmitryesten@gmail.com>
2017-10-06 21:19:28 +0600

    Create generic class NamedArrayBox

---
+++ src/main/java/net/thumbtack/denisenko/trainee/figure/box/NamedArrayBox.java
@@ -0,0 +1,21 @@
+package net.thumbtack.denisenko.trainee.figure.box;
+
+import net.thumbtack.denisenko.trainee.figure.geometry.geometry2D.Rectangle;
+
+public class NamedArrayBox<T> extends ArrayBox {
+
+    private String nameBox;
+
+    public NamedArrayBox(Rectangle[] content) {
+        super(content);
+    }
+
+    public String getNameBox() {
+        return nameBox;
+    }
+
+    public void setNameBox(String nameBox) {
+        this.nameBox = nameBox;
+    }
+
+}
