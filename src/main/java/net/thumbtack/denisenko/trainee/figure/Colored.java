84ea0436ab439052d15ce5023cd51b691c5bd311
Dmitry Denisenko <dmitryesten@gmail.com>
2017-10-08 15:27:37 +0600

    Create exception for method

--- src/main/java/net/thumbtack/denisenko/trainee/figure/Colored.java
+++ src/main/java/net/thumbtack/denisenko/trainee/figure/Colored.java
@@ -1,10 +1,12 @@
 package net.thumbtack.denisenko.trainee.figure;
 
+import net.thumbtack.denisenko.trainee.exceptions.ColorException;
+
 public interface Colored {
 
     String getColor();
 
     void setColor(Color color);
-    void setColor(String color);
+    void setColor(String color) throws ColorException;
 
 }
