9d37d696f037c38d211a0b095b75d175634b1d6e
Dmitry Denisenko <dmitryesten@gmail.com>
2017-10-16 20:12:53 +0600

    Add assertTest for color

--- src/test/java/net/thumbtack/denisenko/trainee/figure/ColorTest.java
+++ src/test/java/net/thumbtack/denisenko/trainee/figure/ColorTest.java
@@ -1,6 +1,7 @@
 package net.thumbtack.denisenko.trainee.figure;
 
 import net.thumbtack.denisenko.trainee.exceptions.ColorException;
+import net.thumbtack.denisenko.trainee.figure.geometry.geometry2D.Circle;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,6 +14,10 @@ public class ColorTest {
         assertEquals(Color.BLACK, Color.colorFromString("BLACK"));
         assertEquals(Color.GREEN, Color.colorFromString("green"));
         assertNotEquals(Color.GREEN, Color.colorFromString("grean"));
+
+        Circle circle = new Circle(1,1,1, (String) null);
+        assertEquals(null, circle.getColor());
+
     }
 
 }
