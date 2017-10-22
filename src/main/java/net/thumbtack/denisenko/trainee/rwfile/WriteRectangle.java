4a2a7056ee14acb92f73aed861fea21504b107b6
Dmitry Denisenko <dmitryesten@gmail.com>
2017-10-22 15:30:54 +0600

    Write byte-to-byte

--- src/main/java/net/thumbtack/denisenko/trainee/rwfile/WriteRectangle.java
+++ src/main/java/net/thumbtack/denisenko/trainee/rwfile/WriteRectangle.java
@@ -1,24 +1,24 @@
 package net.thumbtack.denisenko.trainee.rwfile;
 
 import net.thumbtack.denisenko.trainee.figure.geometry.geometry2D.Rectangle;
-
 import java.io.*;
 
 public class WriteRectangle {
 
     public static void writeRectangle(Rectangle r, File file) throws IOException {
-        try(DataOutputStream dout = new DataOutputStream( new FileOutputStream(file.getAbsoluteFile()))) {
-            String binaryString = WriteRectangle.stringToBinary(r.toString());
-            dout.writeBytes(binaryString);
-            dout.flush();
-            dout.close();
+        try(DataOutputStream dout = new DataOutputStream( new FileOutputStream(file))) {
+            byte [] bytes = r.toString().getBytes();
+            for (byte i : bytes) {
+                dout.write(i);
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
 
-    public static String stringToBinary(String s) {
+
+    private static String stringToBinary(String s) {
         StringBuilder sourceString = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
