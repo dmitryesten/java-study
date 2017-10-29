0ad67ecdd9711238d5b897dbc9f1e6fc3ffc0f00
Dmitry Denisenko <dmitryesten@gmail.com>
2017-10-29 15:32:03 +0600

    Test js

--- frontend/lesson_1/task_1.js
+++ frontend/lesson_1/task_1.js
@@ -1,1 +1,36 @@
-console.log("Hellow World");
+var readlineSync = require('readline-sync');
+console.log("Игра крестики нолики");
+console.log("Игра "+"крестики "+"нолики");
+
+var userName;
+var matrix = [];
+var statusElementMatrix = {
+    e : '#',
+    x : 'x',
+    o : 'o'
+};
+
+userName = readlineSync.question('Enter your name: ');
+    console.log('Hi ' + userName + '!');
+
+
+
+
+matrix = createArray(3, 3);
+console.log("\r\n");
+console.log(print(matrix));
+
+
+function createArray (row, col){
+    for(var i = 0; i < row; i++){
+        matrix[i] = new Array(3);
+        for(var j = 0; j < col; j++)
+            matrix[i] = statusElementMatrix.e;
+    }
+    return matrix;    
+};
+
+function print(matrix){
+    for(var i = 0; i < matrix.length; i++)
+            console.log(matrix);
+};
