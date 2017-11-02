b94fa19d32da9652911b7236d695c58a89e2e565
Dmitry Denisenko <dmitryesten@gmail.com>
2017-11-02 21:57:40 +0600

    Add ByteBufferReader

--- frontend/lesson_1/task_1.js
+++ frontend/lesson_1/task_1.js
@@ -3,33 +3,53 @@ console.log("Tik-tok play");
 console.log("Tik-tok "+"play "+ " ");
 
 var userName;
-var matrix = [];
+var matrix = new Array(3);
 var statusElementMatrix = {
-    e : '#',
+    e : ' ',
     x : 'x',
     o : 'o'
 };
 
-userName = readlineSync.question('Enter your name: ');
-    console.log('Hi ' + userName + '!');
+//userName = readlineSync.question('Enter your name: ');
+  //  console.log('Hi ' + userName + '!');
+
+matrix = createArray();
+    console.log(print(matrix));
+    matrix = set(1, 2);
+    console.log(matrix);
 
 
 
-matrix = createArray(3, 3);
-console.log("\r\n");
-console.log(print(matrix));
+function start(){
+    var stepUser;
+    readlineSync.promptLoop(function(input, input2) {
+      console.log('-- You said "' + input + '" '+ input2+' sad');
+      return input === 'q';
+    });
+    console.log('It\'s exited from loop.'); };
 
 
-function createArray (row, col){
-    for(var i = 0; i < row; i++){
+function createArray (){
+    for(var i = 0; i < 3; i++){
         matrix[i] = new Array(3);
-        for(var j = 0; j < col; j++)
+        for(var j = 0; j < 3; j++)
             matrix[i] = statusElementMatrix.e;
     }
-    return matrix;    
-};
+        return matrix; };
 
 function print(matrix){
     for(var i = 0; i < matrix.length; i++)
-            console.log(matrix);
+            console.log(matrix);    };
+
+function set(x, y){
+    if(!(+x >= 1 && +x <= 3) && !(+y >= 1 && +y <= 3))
+            throw new SyntaxError("Параметр 'x'or'y' некорректны");
+    
+    for(var i = 0; i < matrix.length; i++){
+        for(var j = 0; j < matrix[i].length; j++){
+                matrix[i][j] = statusElementMatrix.x;
+        }
+    }
+
+    return matrix;
 };
