fa4e65ed1c51c1a539b9ed6a4619200c20926ea0
Dmitry Denisenko <dmitryesten@gmail.com>
2017-11-02 21:56:02 +0600

    Change class for reading

--- src/main/java/net/thumbtack/denisenko/trainee/io/read/ReadRectangle.java
+++ src/main/java/net/thumbtack/denisenko/trainee/io/read/ReadRectangle.java
@@ -1,18 +1,18 @@
 package net.thumbtack.denisenko.trainee.io.read;
 
+import net.thumbtack.denisenko.trainee.exceptions.FileException;
+
 import java.io.*;
 
 public class ReadRectangle {
 
-    public static void read(File file) throws IOException {
-        try(InputStream dinput = new FileInputStream(file)) {
-        	// REVU you do not need to read bytes, also converting them to char is wrong idea
+    public static void read(File file) throws IOException, FileException {
+        try(DataInputStream dinput = new DataInputStream(new FileInputStream(file))) {
             while(dinput.available() > 0) {
-                System.out.print((char)dinput.read());
-                //System.out.print( dinput.read() );
+                System.out.print( dinput.read() );
             }
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            throw new FileException(file);
         }
     }
 
