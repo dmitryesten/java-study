74d096e0e92d6ce0efab58779bcc984e5eed27c2
Dmitry Denisenko <dmitryesten@gmail.com>
2017-11-18 23:27:04 +0600

    Add task_5

---
+++ frontend/lesson_3/task_5.js
@@ -0,0 +1,113 @@
+'use strict';
+const readline = require('readline');
+
+let set = new Set();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter number: ', (number) => {
+    if (number != +number){
+        rl.close(); throw new Error("Chars aren't valide");
+    }
+    else{
+        set.add(number);
+        rl.question('Enter number: ', (number) => {
+            if (number != +number){
+                rl.close(); throw new Error("Chars aren't valide");
+            }
+            else{
+            set.add(number);
+            rl.question('Enter number: ', (number) => {
+                if (number != +number){
+                    rl.close(); throw new Error("Chars aren't valide");
+                }
+                else{
+                set.add(number);
+                rl.question('Enter number: ', (number) => {
+                    if (number != +number){
+                        rl.close(); throw new Error("Chars aren't valide");
+                    }
+                    else{
+                    set.add(number);
+                    rl.question('Enter number: ', (number) => {
+                        if (number != +number){
+                            rl.close(); throw new Error("Chars aren't valide");
+                        }
+                        else{
+                        set.add(number);
+                        rl.question('Enter number: ', (number) => {
+                            if (number != +number){
+                                rl.close(); throw new Error("Chars aren't valide");
+                            }
+                            else{
+                            set.add(number);
+                            rl.question('Enter number: ', (number) => {
+                                if (number != +number){
+                                    rl.close(); throw new Error("Chars aren't valide");
+                                }
+                                else{
+                                set.add(number);
+                                rl.question('Enter number: ', (number) => {
+                                    if (number != +number){
+                                        rl.close(); throw new Error("Chars aren't valide");
+                                    }
+                                    else{
+                                    set.add(number);
+                                    rl.question('Enter number: ', (number) => {
+                                        if (number != +number){
+                                            rl.close(); throw new Error("Chars aren't valide");
+                                        }
+                                        else{
+                                        set.add(number);
+                                        rl.question('Enter number: ', (number) => {
+                                            if (number != +number){
+                                                rl.close(); throw new Error("Chars aren't valide");
+                                            }
+                                            else{
+                                            set.add(number);
+                                                maxValue(set);
+                                            rl.close();
+                                            }
+                                        });
+                                        }
+                                    });
+                                    }
+                                });
+                                }
+                            });
+                            }
+                        });
+                        }
+                    });
+                    }
+                });
+                }
+            });
+            }
+        });
+        }
+    });
+
+
+function maxValue(set){
+    let array = [];
+    for (let v of set) {
+        array.push(v)
+    }
+    console.log("Max element: "+Math.max(...array));
+};
+
+//let recursiveAsyncReadLine = function() {
+//   rl.question('Command: ', function (answer) {
+//        if (answer !== +answer) //we need some base case, for recursion
+//            return rl.close(); //closing RL and returning from function.
+//        set.add(answer);
+//            console.log('Got it!"');
+//        recursiveAsyncReadLine(); //Calling this function again to ask new question
+//    });
+
+//};
+//recursiveAsyncReadLine();
