4450b8aac303c9314375924a98bb8e35702fd2e0
Dmitry Denisenko <dmitryesten@gmail.com>
2017-12-01 02:07:54 +0600

    Add function is creating option-list of js

--- frontend/lesson_4/task1.js
+++ frontend/lesson_4/task1.js
@@ -1,28 +1,70 @@
 'use strict';
 
-const [tName, tCreate, tRefresh, tRemove, tCategory] = getValuesAddedTask;
-let name = nameTask;
-let create = tCreate;
-let refresh = tRefresh;
-let category = tCategory;
+let mapCategories = new Map();
+mapCategories.set(1, "Study");
+mapCategories.set(2, "Home");
+mapCategories.set(3, "Job");
 
+let newTaskLi = document.createElement('li');
+let selectedOptionText;
+const addButton = document.getElementById('add-task');
+const categoryTagSelect = document.getElementById('categories');
 
-let getTimeCreate = () => {
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+
+   createOptions(mapCategories);
+   
+  });
+
+function createOptions(){
+    for (var [key, value] of mapCategories) {
+        var oOption = document.createElement("option");
+        oOption.appendChild(document.createTextNode(value));
+        oOption.setAttribute("value", key);
+        categoryTagSelect.appendChild(oOption);
+    }
+}
+
+function getTimeCreate () {
     let date = new Date();
-    return date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+    return date.getFullYear()+'-'+date.getMonth()+'-'+date.getDay()+' '+date.getHours()+':'+date.getMinutes();
 };
 
 let setTime = sel => {  
     document.getElementById(sel).innerText = getTimeCreate; 
 }
 
+//Get array parameters of new task (name's task, date, date, date, category's task)
 function getValuesAddedTask(){
     let array = [];
     var list = document.getElementsByClassName("form-task__field");
     for (var i = 0; i < list.length; i++) {
         array.push(list[i].value);
     }
-     
     return array;    
 }
+
+addButton.addEventListener('click', function(e){
+    document.getElementById('date-create').value = getTimeCreate();
+    selectedOptionText = getTextCurrentOption(categoryTagSelect);
+
+    let listTask = document.getElementById('ltask');
+    newTaskLi.innerHTML = Math.random();
+    listTask.insertBefore(newTaskLi, listTask.firstChild);
+    newTaskLi.innerHTML = Math.random();
+    listTask.insertBefore(newTaskLi, listTask.firstChild);
+  
+});
+
+
+function getTextCurrentOption(categorySelect){
+    let textOption;
+    if ( categorySelect.selectedIndex != -1){
+      textOption = categorySelect.options[categorySelect.selectedIndex].text;
+    }
+    return textOption;
+};
+
+
 
